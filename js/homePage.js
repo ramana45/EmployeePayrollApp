7a8a6a5ce1e1220d34b5120b8aede03ea92d3fb5
ramana <ramanarajupyla@gmail.com>
2021-01-07 20:49:02 +0530

    [Ramana]Added : check for update and set the values for form elemets

--- js/homePage.js
+++ js/homePage.js
@@ -24,7 +24,7 @@ const createInnerHtml = () => {
             <td>${empPayrollData._gender}</td>
             <td>${getDeptHtml(empPayrollData._department)}</td>
             <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._startDate}</td>
+            <td>${stringifyDate(empPayrollData._startDate)}</td>
             <td>
                 <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                 <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
@@ -41,4 +41,23 @@ const getDeptHtml = (deptList) => {
          deptHtml = `${deptHtml}<div class='dept-label'>${dept}</div>`
      }
      return deptHtml;
+};
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if(!empPayrollData) return;
+    const index = empPayrollList
+                    .map(empData => empData._id)
+                    .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+}
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if (!empPayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
+    window.location.replace(site_properties.add_emp_payroll_page);
 };
--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,3 +1,6 @@
+let isUpdate = false;
+let employeePayrollObj = {};
+
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector("#name");
     const textError = document.querySelector(".text-error");
@@ -20,6 +23,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     salary.addEventListener('input', function(){
       output.textContent = salary.value;
     });
+
+    checkForUpdate();
 });
 
 const save = () => {
@@ -75,6 +80,33 @@ const getInputValueById = (id) => {
     return value;
 }
 
+const setForm = () => {
+    setValue('#name', employeePayrollObj._name);
+    setSelectedValues('[name=profile]', employeePayrollObj._profilePic);
+    setSelectedValues('[name=gender]', employeePayrollObj._gender);
+    setSelectedValues('[name=department]', employeePayrollObj._department);
+    setValue('#salary', employeePayrollObj._salary);
+    setTextValue('.salary-output', employeePayrollObj._salary);
+    setValue('#notes', employeePayrollObj._note);
+    let date = stringifyDate(employeePayrollObj._startDate).split(" ");
+    setValue('#day', date[0]);
+    setValue('#month', date[1]);
+    setValue('#year', date[2]);
+}
+
+const setSelectedValues = (propertyValue, value) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        if(Array.isArray(value)) {
+            if(value.includes(item.value)) {
+                item.checked = true;
+            }
+        }
+        else if (item.value === value)
+        item.checked = true;
+    });
+}
+
 const resetForm = () => {
     setValue('#name', '');
     unsetSelectedValues('[name=profile]');
@@ -82,9 +114,9 @@ const resetForm = () => {
     unsetSelectedValues('[name=department]');
     setValue("#salary", "");
     setValue("#notes", "");
-    setValue("#day","1");
-    setValue("#month","January");
-    setValue("#year", "2020");
+    setSelectedIndex("#day",0);
+    setSelectedIndex("#month",0);
+    setSelectedIndex("#year", 0);
 };
 
 const unsetSelectedValues = (propertyValue) =>{
@@ -102,4 +134,17 @@ const setTextValue = (id, value) => {
 const setValue = (id, value) => {
     const element = document.querySelector(id);
     element.value = value;
+}
+
+const setSelectedIndex = (id, index) => {
+    const element = document.querySelector(id);
+    element.selectedIndex = index;
+}
+
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true : false;
+    if (!isUpdate) return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();
 }
