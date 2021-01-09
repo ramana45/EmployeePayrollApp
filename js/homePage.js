884af39f01b1b84dc0402218caa645d5385e6d13
ramana <ramanarajupyla@gmail.com>
2021-01-09 15:27:14 +0530

    [Ramana]refactored : populating the data in the form on edit

--- js/homePage.js
+++ js/homePage.js
@@ -17,6 +17,7 @@ const createInnerHtml = () => {
                         "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
     let innerHtml = `${headerHtml}`;
     for(const empPayrollData of empPayrollList){
+        empPayrollData._id = empPayrollList.indexOf(empPayrollData);
     innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
