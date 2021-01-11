d152e9f1b7ab742928d357a1890f81a87c510c25
ramana <ramanarajupyla@gmail.com>
2021-01-11 19:42:56 +0530

    [Ramana]added : Add/Update the employee in JSON server

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -43,13 +43,34 @@ const save = (event) => {
     event.stopPropagation();
     try {
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace("../pages/homePage.html");
+        if(site_properties.use_local_storage.match("true")) {
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     } catch (e) {
         alert(e);
         return;
     }
+}
+
+const createOrUpdateEmployeePayroll = () => {
+    let postURL = site_properties.server_url;
+    let methodCall = "POST";
+    if(isUpdate) {
+        methodCall = "PUT";
+        postURL = postURL + employeePayrollObj.id.toString();
+    }
+    makeServiceCall(methodCall, postURL, true, employeePayrollObj)
+        .then(responseText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch(error => {
+            throw error;
+        });    
 }
 
 const setEmployeePayrollObject = () => {
